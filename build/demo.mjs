56c0d67cf36ddaec0a76c683444234dce82c2195
许荣增 <767503979@qq.com>
2021-03-10 19:22:32 +0800

    node 处理cjs和es6模块，记得用高版本node，https://www.ruanyifeng.com/blog/2020/08/how-nodejs-use-es6-module.html

---
+++ build/demo.mjs
@@ -0,0 +1,8 @@
+//https://www.ruanyifeng.com/blog/2020/08/how-nodejs-use-es6-module.html
+import fun from './demo3.mjs';
+console.log('fun',fun)
+// const express = require('express')
+// console.log('express',express)
+
+// const {obj}=require('./demo2');
+// console.log('obj',obj)
